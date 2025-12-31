5b77a575ba9ba51ad140aa41f6e23421758b36b7
tianzhi <tianzhi@com>
2025-12-31 11:10:17 +0800

    add：优化GPIO配置和任务延时方式

--- main/freertos_c3_demo.c
+++ main/freertos_c3_demo.c
@@ -8,23 +8,37 @@ static const char *TAG = "RTOS_C3";
 void task1(void *arg)
 {
     gpio_config_t io_conf;
+    esp_err_t res;
+    uint32_t staute = 0;
+    TickType_t last = xTaskGetTickCount();      // 绝对计时定时器
+
     io_conf.pin_bit_mask =  (1ULL << GPIO_NUM_1);
     io_conf.mode = GPIO_MODE_OUTPUT;
-    esp_err_t res = gpio_config(&io_conf);
-    uint32_t staute = 0;
+    res = gpio_config(&io_conf);
+
     if(res == ESP_OK)
     {
-        ESP_LOGI(TAG, "GPIO configured");
+        ESP_LOGI(TAG, "GPIO configured successfully!");
         gpio_set_level(GPIO_NUM_1, staute);
     }
-    while (1) {
+    else
+    {
+        ESP_LOGE(TAG, "GPIO configuration failed!");
+    }
+    while (1) 
+    {
         ESP_LOGI(TAG, "Task1 running on C3");
         if(res == ESP_OK)
         {
             staute = 1UL - staute;
             gpio_set_level(GPIO_NUM_1, staute);
         }
-        vTaskDelay(pdMS_TO_TICKS(5000));
+        // | API             | 用途                 |
+        // | --------------- | ----------           |
+        // | vTaskDelay      | 相对延时             |
+        // | vTaskDelayUntil | 绝对周期（强烈推荐） |
+        // vTaskDelay(pdMS_TO_TICKS(5000));
+        vTaskDelayUntil(&last, pdMS_TO_TICKS(4000));
     }
 }
 
